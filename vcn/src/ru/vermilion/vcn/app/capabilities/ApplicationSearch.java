1f72b7eb2c55ae6077ee6941449c3a598ffa3d72
Alexander Drobyshevsky <drobyshevsky@gmail.com>
2013-11-07 01:54:01 +0400

    Issue 29: Global Search

---
+++ vcn/src/ru/vermilion/vcn/app/capabilities/ApplicationSearch.java
@@ -0,0 +1,71 @@
+package ru.vermilion.vcn.app.capabilities;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+
+import ru.vermilion.vcn.app.VermilionCascadeNotebook;
+
+public class ApplicationSearch implements ICapability {
+	
+	private GlobalSearch globalSearch;
+	
+	private PageSearch pageSearch;
+
+	
+	public ApplicationSearch(Menu submenu, VermilionCascadeNotebook vermilionCascadeNotebook) {
+		globalSearch = new GlobalSearch(vermilionCascadeNotebook);
+		pageSearch = new PageSearch(vermilionCascadeNotebook);
+		
+		addPageSearchMenuItems(submenu);
+		addGlobalSearchMenuItems(submenu);
+	}
+
+	
+	private void addPageSearchMenuItems(Menu submenu) {
+		MenuItem pageSearchItem = new MenuItem (submenu, SWT.PUSH);
+		pageSearchItem.addListener (SWT.Selection, new Listener () {
+			public void handleEvent(Event e) {
+				pageSearch.pageSearchAction();
+			}
+		});
+		pageSearchItem.setText ("Page Find.. \tCtrl + F");
+		pageSearchItem.setAccelerator(SWT.MOD1 | 'F');
+		
+		MenuItem pageReSearchItem = new MenuItem (submenu, SWT.PUSH);
+		pageReSearchItem.addListener (SWT.Selection, new Listener () {
+			public void handleEvent(Event e) {
+				pageSearch.pageReSearchAction();
+			}
+		});
+		pageReSearchItem.setText ("Page Find Next\tF3");
+		pageReSearchItem.setAccelerator(SWT.F3);
+	}
+	
+	private void addGlobalSearchMenuItems(Menu submenu) {
+		MenuItem pageSearchItem = new MenuItem (submenu, SWT.PUSH);
+		pageSearchItem.addListener (SWT.Selection, new Listener () {
+			public void handleEvent(Event e) {
+				globalSearch.globalSearchAction();
+				pageSearch.setSearchParameters(globalSearch.getLastSearchText(), -1);
+				pageSearch.pageReSearchAction();
+			}
+		});
+		pageSearchItem.setText ("Global Find.. \tCtrl + H");
+		pageSearchItem.setAccelerator(SWT.MOD1 | 'H');
+		
+		MenuItem pageReSearchItem = new MenuItem (submenu, SWT.PUSH);
+		pageReSearchItem.addListener (SWT.Selection, new Listener () {
+			public void handleEvent(Event e) {
+				globalSearch.globalReSearchAction();
+				pageSearch.setSearchParameters(globalSearch.getLastSearchText(), -1);
+				pageSearch.pageReSearchAction();
+			}
+		});
+		pageReSearchItem.setText ("Global Find Next\tF4");
+		pageReSearchItem.setAccelerator(SWT.F4);
+	}
+	
+}
