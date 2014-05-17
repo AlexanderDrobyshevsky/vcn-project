fbaff7e10ad711717027514ef75bb2556fa10435
unknown <drobyshevsky@gmail.com>
2014-05-17 18:21:14 +0300

    Task 65. 65. Add global fault message for case of program global fatal errors (like 'Cannot load 64-bit SWT libraries on 32-bit JVM')

--- vcn/src/ru/vermilion/vcn/app/ApplicationStart.java
+++ vcn/src/ru/vermilion/vcn/app/ApplicationStart.java
@@ -1,5 +1,7 @@
 package ru.vermilion.vcn.app;
 
+import javax.swing.JOptionPane;
+
 import org.eclipse.swt.widgets.Shell;
 
 import ru.vermilion.vcn.auxiliar.GeneralUtils;
@@ -8,26 +10,31 @@ import ru.vermilion.vcn.auxiliar.UI;
 public class ApplicationStart {
 
 	public static void main(String [] args) {
-		VermilionCascadeNotebook vermilionCascadeNotebook = VermilionCascadeNotebook.getInstance();
-		vermilionCascadeNotebook.init();
-
-		Shell vcnShell = vermilionCascadeNotebook.getMainComposite().getShell();
-		vcnShell.open();
-		while (!vcnShell.isDisposed()) {
-			try {
-				if (!vcnShell.getDisplay().readAndDispatch ()) vcnShell.getDisplay().sleep ();
-			} catch (Exception ex) {
-				if (!vcnShell.getShell().isDisposed()) {
-					UI.messageDialog(VermilionCascadeNotebook.getInstance().getShell(), "Program fatal error (" + ex + ")", 
-							"Program fatal error: " + ex +"\r\nError stack: \r\n\r\n" + GeneralUtils.getStackTrace(ex));
-				} 
-				
-				ex.printStackTrace();
+		try {
+			VermilionCascadeNotebook vermilionCascadeNotebook = VermilionCascadeNotebook.getInstance();
+			vermilionCascadeNotebook.init();
+	
+			Shell vcnShell = vermilionCascadeNotebook.getMainComposite().getShell();
+			vcnShell.open();
+			while (!vcnShell.isDisposed()) {
+				try {
+					if (!vcnShell.getDisplay().readAndDispatch ()) vcnShell.getDisplay().sleep ();
+				} catch (Exception ex) {
+					if (!vcnShell.getShell().isDisposed()) {
+						UI.messageDialog(VermilionCascadeNotebook.getInstance().getShell(), "Program fatal error (" + ex + ")", 
+								"Program fatal error: " + ex +"\r\nError stack: \r\n\r\n" + GeneralUtils.getStackTrace(ex));
+					} 
+					
+					ex.printStackTrace();
+				}
 			}
-		}
-		
-		if (!vcnShell.isDisposed()) {
-			vcnShell.getDisplay().dispose();
+			
+			if (!vcnShell.isDisposed()) {
+				vcnShell.getDisplay().dispose();
+			}
+		} catch (Throwable te) {
+			JOptionPane.showMessageDialog(null, te.getMessage() + "\r\n" + 
+						"Program terminates", "Program Fault", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 }
